929dc5820b1db78bff185c5ccad9ee025a7cc76e
JonksPanda <jionte@gmail.com>
2022-12-09 10:05:25 +0100

    Added class character, player and monster

--- characters.py
+++ characters.py
@@ -1,10 +1,31 @@
 class character:
-    pass
+    def __init__(self, type=''):
+        self.initiative = 0
+        self.health = 0
+        self.attack = 0
+        self.dexterity = 0
 
 
 class player(character):
-    pass
+    def __init_subclass__(cls, self, points=0):
+        self.points = points
+        self.ability = ''
+        self.generate_class()
+        return super().__init_subclass__()
+
+    def generate_class(self):
+        pass
+
+# create subclass for every character class
 
 
 class monster(character):
-    pass
+    def __init_subclass__(cls, self):
+        self.probability = 0
+        self.generate_class()
+        return super().__init_subclass__()
+
+    def generate_class(self):
+        pass
+
+# create subclass for every type of monster
