fc64fe1fbbafaaceeec6103ea3e13ce1f2814989
JonksPanda <jionte@gmail.com>
2022-12-11 21:37:09 +0100

    added new menu functions and modified character classes

--- characters.py
+++ characters.py
@@ -1,4 +1,4 @@
-class character:
+class Character:
     def __init__(self, type=''):
         self.initiative = 0
         self.health = 0
@@ -6,25 +6,28 @@ class character:
         self.dexterity = 0
 
 
-class player(character):
-    def __init_subclass__(cls, self, points=0):
+class Player(Character):
+    def __init_subclass__(cls, self, name, points=0):
         self.points = points
+        self.name = name
         self.ability = ''
         self.generate_class()
         return super().__init_subclass__()
 
+    # sets stats and ability according to stats
     def generate_class(self):
         pass
 
 # create subclass for every character class
 
 
-class monster(character):
+class Monster(Character):
     def __init_subclass__(cls, self):
         self.probability = 0
         self.generate_class()
         return super().__init_subclass__()
 
+    # sets stats and probability according to class
     def generate_class(self):
         pass
 
--- menu_functions.py
+++ menu_functions.py
@@ -1,14 +1,57 @@
-def set_playername():
-    pass
+import characters
 
 
 def set_player_class():
-    pass
+    player_classes = {"1": "knight", "2": "wizard", "3": "thief"}
+    while True:
+        selected_class = input("choose your class (1-3): ")
+        if selected_class in player_classes.keys():
+            print(f"selected class is {player_classes[selected_class]}")
+            return player_classes[selected_class]
+        else:
+            print("wrong input!")
 
 
-def set_room_size():
-    pass
+def set_playername():
+    name = input("name: ")
+    return name
+
+
+def set_map_size():
+    maps = {"1": (4, 4), "2": (5, 5), "3": (8, 8)}
+    while True:
+        room_size = input("select map size (1-3): ")
+        if room_size in maps.keys():
+            print(f"map size is {maps[room_size][0]}x{maps[room_size][1]}")
+            return maps[room_size]
+        else:
+            print("wrong input!")
+
+
+def select_option():
+    while True:
+        selected_option = input("select option (1-2): ")
+        if selected_option == "1":
+            return game_setup()
+        elif selected_option == "2":
+            return get_stats()
+        else:
+            print("wrong input!")
+
+
+def game_setup():
+    name = set_playername()
+    # check if player already exists
+    # else, create new
+    player_class = set_player_class()
+    map_size = set_map_size()
+    player = characters.Player(name=name, type=player_class)
 
 
 def get_stats():
+    # print stats
     pass
+
+
+if __name__ == "__main__":
+    select_option()
