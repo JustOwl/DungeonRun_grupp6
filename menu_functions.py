294e0dc5a937c1e108df8b264e7abaa485e1e424
jonkspanda <jionte@gmail.com>
2022-12-12 13:56:08 +0100

    Updated menu_functions.py

--- menu_functions.py
+++ menu_functions.py
@@ -23,13 +23,14 @@ def set_map_size():
             print("wrong input!")
 
 
-def select_option():
+def set_start_pos():
     while True:
-        selected_option = input("select option (1-2): ")
-        if selected_option == "1":
-            return game_setup()
-        elif selected_option == "2":
-            return get_stats()
+        positions = {"1": "top left", "2": "top right ",
+                     "3": "bottom left", "4": "bottom right"}
+        starting_pos = input("choose a starting position (1-4): ")
+        if starting_pos in positions:
+            print(f"starting postion is {positions[starting_pos]}")
+            return int(starting_pos)
         else:
             print("wrong input!")
 
@@ -39,13 +40,28 @@ def game_setup():
     # else, create new
     player_class = set_player_class()
 
+    # creates player and generates all stats according to selected class
     player = characters.Player(type=player_class)
+    # returns int with map size
     map_size = set_map_size()
+    # returns int(1-4) with starting position
+    start_pos = set_start_pos()
 
 
 def get_stats():
     # print stats
     pass
+
+
+def select_option():
+    while True:
+        selected_option = input("select option (1-2): ")
+        if selected_option == "1":
+            return game_setup()
+        elif selected_option == "2":
+            return get_stats()
+        else:
+            print("wrong input!")
 
 
 if __name__ == "__main__":
