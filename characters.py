43d6f25f327ff20ed6a8476867c26207a936fdbd
Jonathan Friberg <jionte@gmail.com>
2022-12-12 12:31:21 +0100

    modified characters.py

--- characters.py
+++ characters.py
@@ -1,34 +1,31 @@
 class Character:
     def __init__(self, type=''):
+        super().__init__()
         self.initiative = 0
         self.health = 0
         self.attack = 0
         self.dexterity = 0
+        self.type = type
 
 
 class Player(Character):
-    def __init_subclass__(cls, self, name, points=0):
+    def __init__(self, name, type, points=0):
+        super().__init__(type)
         self.points = points
         self.name = name
         self.ability = ''
         self.generate_class()
-        return super().__init_subclass__()
 
-    # sets stats and ability according to stats
     def generate_class(self):
         pass
 
-# create subclass for every character class
-
 
 class Monster(Character):
-    def __init_subclass__(cls, self):
+    def __init__(self, type):
+        super().__init__(type)
         self.probability = 0
         self.generate_class()
-        return super().__init_subclass__()
 
     # sets stats and probability according to class
     def generate_class(self):
         pass
-
-# create subclass for every type of monster
