16265bb7a4643ce61c6752636b242b37d0d53e6d
jonkspanda <jionte@gmail.com>
2022-12-13 08:54:13 +0100

    edited characters.py

--- characters.py
+++ characters.py
@@ -20,19 +20,16 @@ class Player(Character):
         types = {"knight": (5, 9, 6, 4, "shield block"), "wizard": (
             6, 4, 9, 5, "light beam"), "thief": (7, 5, 5, 7, "critical hit")}
 
-        self.initiative = types[self.type][0]
-        self.health = types[self.type][1]
-        self.attack = types[self.type][2]
-        self.dexterity = types[self.type][3]
-        self.ability = types[self.type][4]
+        self.initiative, self.health, self.attack, self.dexterity, self.ability = types[
+            self.type]
 
 
 class Monster(Character):
     def __init__(self, type):
         super().__init__(type)
-        self.probability = 0
+        # self.probability = 0
         self.generate_class()
 
-    # sets stats and probability according to class
+    # sets stats according to type
     def generate_class(self):
         pass
